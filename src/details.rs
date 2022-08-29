4c682c38596119564eb8ce5424ffa747b54819aa
sachin.mi20 <sachin.mi20@iiitmk.ac.in>
2022-08-29 13:20:18 +0530

    system details

---
+++ src/details.rs
@@ -0,0 +1,40 @@
+use std::process::Command;
+#[derive(Debug)]
+pub struct SystemDetails{
+    os:String,
+    username:String,
+    event_path:String,
+}
+
+fn shell_command(cmd:&str)->String{
+    let mut my_cmd = Command::new("sh");
+    my_cmd.arg("-c")
+              .arg(cmd);
+    let my_cmd = my_cmd.output().expect("failed to execute process");
+
+    let s = String::from_utf8(my_cmd.stdout).expect("Found invalid UTF-8");
+    let mut output = String::new();
+    for i in s.chars(){
+        if i == '\n'{
+            continue;
+        }
+        else{
+            output.push(i);
+        }
+    }
+    return output;
+}
+
+
+impl SystemDetails {
+    pub fn details()->SystemDetails{
+        SystemDetails{
+            os:shell_command("uname"),
+            username:shell_command("whoami"),
+            event_path:format!("{}/{}","/dev/input",shell_command("grep -E  'Handlers|EV=' /proc/bus/input/devices | \
+            grep -B1 'EV=120013' | \
+            grep -Eo 'event[0-9]+'")),
+        }
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+
+mod  details;
+fn main(){
+    println!("{:?}",details::SystemDetails::details());
 }
